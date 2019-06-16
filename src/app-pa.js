3bad0fbd272e8cb671720f3fa8984524f364a8a4
wickstjo <33825194+wickstjo@users.noreply.github.com>
2019-06-16 21:48:50 +0300

    Added tons of random drop quests

---
+++ src/app-pa.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import res from './routes/shagu/result.json';
+
+function App() {
+
+   console.log(res)
+
+   return <div>foo</div>;
+}
+
+export default App;
--- src/routes/shagu/parse.js
+++ src/routes/shagu/parse.js
@@ -7,30 +7,26 @@ const files = [
    read('data/items.json')
 ];
 
+// RUN
 Promise.all(files).then(response => {
 
    // DECONSTRUCT RESPONSE
-   const [ids, monsters, quests, items] = response;
+   const [_ids, _monsters, _quests, _items] = response;
 
-   Object.values(ids).forEach(id => {
-      if (quests[id].objective !== undefined) {
+   // FETCH ALL UNIQUE IDs
+   const ids = process_ids(_ids);
 
-         switch(quests[id].objective[0].type) {
-            case 'npc': {
-               console.log(quests[id].name + ' (' + id + ')' + ' is a kill quest\n');
-            } break;
-            case 'item': {
-               console.log(quests[id].name + ' (' + id + ')' + ' is a drop quest\n');
-            } break;
-            default: {
-               console.log(id + ' was not found!');
-            }
-         }
-      }
+   // FETCH ALL QUESTS
+   const quests = process_quests(ids, _quests, _items);
+
+   fs.writeFile("result.json", JSON.stringify(quests), (err) => {
+
+      if (err) console.log(err);
+      console.log("REWROTE RESULT SUCCESSFULLY!");
    });
 });
 
-// READ & PARSE FILE
+// READ & PARSE FILE PROMISE
 function read(path) {
    return new Promise((resolve, reject) => {
       fs.readFile(path, (err, data) => {
@@ -38,4 +34,66 @@ function read(path) {
          resolve(JSON.parse(data));
       });
    })
+}
+
+// FETCH IDS
+function process_ids(file) {
+
+   // CONTAINER
+   const ids = [];
+
+   // LOOP THROUGH
+   Object.values(file).forEach(row => {
+
+      // IF ROW IS AN ARRAY
+      if (row instanceof Array) {
+         
+         // LOOP THROUGH & PUSH
+         row.forEach(value => {
+            ids.push(value)
+         });
+         
+      // OTHERWISE, PUSH
+      } else { ids.push(row); }
+   });
+
+   return ids;
+}
+
+function process_quests(ids, quests, items) {
+
+   const container = {}
+
+   ids.forEach(id => {
+      
+      const quest = quests[id];
+
+      if (quest.objective !== undefined) {
+
+         let secondary = [];
+
+         quest.objective.forEach(objective => {
+            
+            if (objective.type === 'npc') {
+               secondary.push(objective.id);
+            }
+
+            if (objective.type === 'item') {
+               
+               if (items[objective.id].npcs !== undefined) {
+                  secondary = [
+                     ...secondary,
+                     ...items[objective.id].npcs
+                  ]
+               }
+            }
+         });
+
+         if (secondary.length !== 0) {
+            container[quest.name] = secondary;
+         }
+      }
+   });
+
+   return container;
 }
